6556ae991096ac736f3db01c8f48cc4962fcecb9
Basit612 <sheikhabdulbasit612@gmail.com>
2024-05-15 21:29:38 +0600

    banner section add

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,64 @@
-
-
 const Navbar = () => {
-  return (
-    <div>
+  const navOptions = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>CONTACT us</a>
+      </li>
+      <li>
+        <a>DASHBOARD</a>
+      </li>
+      <li>
+        <a>Menu</a>
+      </li>
       
-    </div>
+    </>
+  );
+  return (
+    <>
+      <div className="navbar fixed z-10 bg-opacity-30 bg-[#151515] text-white max-w-screen-xl">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navOptions}
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">
+            BISTRO BOSS <br />
+            Restaurant
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+          {navOptions}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <a className="btn">Button</a>
+        </div>
+      </div>
+    </>
   );
 };
 
